cfb4a40f8901861aaf10033e76ae9afe82c5ab42
Matt Bell <mappum@gmail.com>
2016-04-19 13:22:38 -0700

    Added wallet params

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,51 @@
 function createParams (params, assert) {
   assert = assert != null ? assert : true
   if (assert) {
-    if (!params) {
+    // TODO: check for these properties procedurally
+    if (params == null) {
       throw new Error('Must provide override params')
     }
-    if (!params.blockchain) {
+    if (params.blockchain == null) {
       throw new Error('Must provide blockchain params')
     }
-    if (!params.blockchain.genesisHeader) {
+    if (params.blockchain.genesisHeader == null) {
       throw new Error('Must provide blockchain.genesisHeader')
     }
-    if (!params.net) {
+    if (params.net == null) {
       throw new Error('Must provide net params')
     }
     if (params.net.magic == null) {
       throw new Error('Must provide net.magic')
     }
-    if (!params.net.defaultPort) {
+    if (params.net.defaultPort == null) {
       throw new Error('Must provide net.defaultPort')
+    }
+    if (params.wallet == null) {
+      throw new Error('Must provide wallet params')
+    }
+    if (params.wallet.bip32 == null) {
+      throw new Error('Must provide wallet.bip32')
+    }
+    if (params.wallet.messagePrefix == null) {
+      throw new Error('Must provide wallet.messagePrefix')
+    }
+    if (params.wallet.bip32.public == null) {
+      throw new Error('Must provide wallet.bip32.public')
+    }
+    if (params.wallet.bip32.private == null) {
+      throw new Error('Must provide wallet.bip32.private')
+    }
+    if (params.wallet.pubKeyHash == null) {
+      throw new Error('Must provide wallet.pubKeyHash')
+    }
+    if (params.wallet.scriptHash == null) {
+      throw new Error('Must provide wallet.scriptHash')
+    }
+    if (params.wallet.wif == null) {
+      throw new Error('Must provide wallet.wif')
+    }
+    if (params.wallet.dustThreshold == null) {
+      throw new Error('Must provide wallet.dustThreshold')
     }
   }
 
@@ -26,6 +54,7 @@ function createParams (params, assert) {
     params.blockchain = Object.assign({}, extend.blockchain, child.blockchain)
     delete params.blockchain.checkpoints
     params.net = Object.assign({}, extend.net, child.net)
+    params.wallet = Object.assign({}, extend.wallet, child.wallet)
     return createParams(params, assert)
   }
   return Object.assign(extend, params)
